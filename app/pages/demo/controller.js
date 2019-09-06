4c847aeca3149b50409d781b7a726a8d5e2d2d84
Ernesto Rodriguez <ernesto.rodriguez@mercadolibre.com>
2019-09-05 22:18:15 -0300

    added basic test for routers

--- app/pages/demo/controller.js
+++ app/pages/demo/controller.js
@@ -8,6 +8,6 @@ exports.render = function render(req, res) {
 
     res.send(template({
         body: body,
-        title: 'Hello World from  the server'
+        title: 'Hello World from the server'
     }));
 };
--- tests/unit/app/pages/demo/controller.spec.js
+++ tests/unit/app/pages/demo/controller.spec.js
@@ -4,7 +4,8 @@ import { render } from '../../../../../app/pages/demo/controller';
 describe('demo Page controller', () => {
    it('mus call send with correct params', (done) => {
        render({},{
-           send() {
+           send(response) {
+               expect(response).to.be.an('string');
                done();
            }
        })
